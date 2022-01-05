97f0ab6a9d93f6247d2142d8d8074dba3833e898
Adnan Tahir <adnantahir1998@gmail.com>
2022-01-06 02:25:51 +0500

    Slider has been completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import Slider from './Components/Slider/Slider'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h3>Custome Slider</h3>
+      <p><q>Fully Customizable Slider Carousel using React useState Hook</q></p>
+      <span>Without using any external Library</span>
+      <Slider />
     </div>
   );
 }
